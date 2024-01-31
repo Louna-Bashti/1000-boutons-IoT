30d9ecbb51abdf084eb67cacaf496e92245e5d8e
Louna_Bashti <bashlodu59@orange.fr>
2024-01-31 17:58:52 +0100

    commit final

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,36 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+// Declare the Analog pin on the Arduino board
+int buttonPin = A0;
+void setup()
+{
+  // Establish the Serial connection with a baud rate of 9600
+  Serial.begin(9600);
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+  // Read the value of the input. It can vary from 0 - 1023
+  int buttonValue = analogRead(buttonPin);
+  if (buttonValue < 200)
+  {
+    // A value under 200 represents no button pushed
+    Serial.println("0");
+  }
+  else if (buttonValue >= 200 && buttonValue < 300)
+  {
+    // A value between 200 - 300 represents the third button
+    Serial.println("S3");
+  }
+  else if (buttonValue >= 300 && buttonValue < 400)
+  {
+    // A value between 300 - 400 represents the second button
+    Serial.println("S2");
+  }
+  else if (buttonValue >= 400)
+  {
+    // A value greater than 400 represents the first button
+    Serial.println("S1");
+  }
+  // Delays the execution to allow time for the Serial transmission
+  delay(500);
 }
